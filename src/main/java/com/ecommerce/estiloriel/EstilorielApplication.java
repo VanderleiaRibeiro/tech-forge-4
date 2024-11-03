f8a41a69770476dc4bb38f9bf55efa5b45306979
VanderleiaRibeiro <161781397+VanderleiaRibeiro@users.noreply.github.com>
2024-11-03 19:10:24 -0300

    inicializando projeto

---
+++ src/main/java/com/ecommerce/estiloriel/EstilorielApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.estiloriel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstilorielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstilorielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/estiloriel/EstilorielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.estiloriel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstilorielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
